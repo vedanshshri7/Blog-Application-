4d7ab4d12eaac2c90f3f95047a772ca978cf0eba
Vedansh Shrivastava <vedanshshrivastava3@gmail.com>
2024-08-01 19:47:27 +0530

    added get own all category

--- routes/category.js
+++ routes/category.js
@@ -3,16 +3,18 @@ const router = express.Router()
 const Category = require('../model/Category')
 const mongoose = require('mongoose')
 const checkAuth = require('../middleware/checkAuth')
+const jwt = require('jsonwebtoken')
 
 //add category
 
 router.post('/',checkAuth,(req,res)=>{
 
-
+  const token = req.headers.authorization.split(" ")[1]
+  const verify =  jwt.verify(token,'ved 147')
 
 const newCategory = new Category({
   _id: new mongoose.Types.ObjectId,
-  userId:req.body.userId,
+  userId:verify.userId,
   title:req.body.title,
   imageUrl:req.body.imageUrl,
 
@@ -35,6 +37,24 @@ const newCategory = new Category({
 
 /// get all Category
 
+router.get('/',checkAuth,(req,res)=>{
+  const token = req.headers.authorization.split(" ")[1]
+  const verify =  jwt.verify(token,'ved 147')
+  Category.find({userId:verify.userId})
+
+  .select("_id userId title imageUrl")
+  .then(result=>{
+    res.status(200).json({
+      categoryList:result
+    })
+  })
+  .catch(err=>{
+    console.log(err)
+    res.status(500).json({
+      error:TypeError
+    })
+  })
+})
 
 
 module.exports = router
