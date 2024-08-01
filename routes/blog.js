c7c3d6091ddf1b063382775bee7f428a1945e591
Vedansh Shrivastava <vedanshshrivastava3@gmail.com>
2024-08-01 23:53:46 +0530

    getall,update blog

--- routes/blog.js
+++ routes/blog.js
@@ -75,6 +75,26 @@ router.get('/',checkAuth,(req,res)=>{
     })
   })
 })
+////
+//get blogs by category
+router.get('/getBycategory/:id',(req,res)=>{
+  Blog.find({categoryId:req.params.id})
+  .select("_id userId categoryId categoryTitle title imageUrl userName")
+  .then(result=>{
+    res.status(200).json({
+      blogs:result
+    })
+  })
+  .catch(err=>{
+    console.log(err)
+    res.status(500).json({
+      error : err
+    })
+  })
+
+})
+
+
 
 
 ////delete own blog
@@ -107,6 +127,48 @@ router.delete('/:id',(req,res)=>{
     })
   })
 })
+
+
+/// update own blog
+
+
+router.put('/:id',checkAuth,(req,res)=>{
+  const token = req.headers.authorization.split(" ")[1]
+  const verify =  jwt.verify(token,'ved 147')
+  console.log(verify)
+
+  Blog.find({_id:req.params.id,userId:verify.userId})
+    .then(result=>{
+      if(result.length == 0){
+        return res.status(400).json({
+          msg:'something is wrong'
+        })
+      }
+      Blog.findOneAndUpdate({_id:req.params.id,userId:verify.userId},{
+        $set:{
+          userId:verify.userId,
+        title:req.body.title,
+        imageUrl:req.body.imageUrl,
+        categoryId:req.body.categoryId,
+        categoryTitle:req.body.categoryTitle,
+         blogDetail:req.body.blogDetail,
+        userName:verify.firstName +" " + verify.lastName
+        }
+      })
+        .then(result=>{
+          res.status(200).json({
+            msh:result
+          })
+        })
+        .catch(err=>{
+          console.log(err)
+          res.status(500).json({
+            error : err
+          })
+        })
+      })
+})
+
 
 
 
